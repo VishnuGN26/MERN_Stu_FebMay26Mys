51da1857d4ac1ad5c131e421754135cf8adc28ba
VishnuGN26 <vishnugn145@gmail.com>
2026-03-03 15:42:39 +0530

    adding

---
+++ W3/D2/p4.js
@@ -0,0 +1,31 @@
+//conditonal statements
+// let age = 15;
+// if(age < 13){
+//     console.log("Child");
+// }
+// else if(age<18)
+//     console.log("Teenager");
+// else
+// {
+//     console.log("Adult");
+// }
+
+// //switch statements
+// console.log("Switch statements");
+// let day = "C";
+// switch ("C") {
+//     case "Monday":
+//         console.log("Start of the week");
+//         break;
+//         case "Wednesday":
+//             console.log("Mid of th week");
+//             break;
+//         case "Friday": 
+//         console.log("End of work week");
+//         default:
+//             console.log("Some day in the week");
+//             break;
+// }
+//Type coversion
+let n = Number("ABC");
+console.log("n: ",n,"Type of n:",typeof n,"isNaN",isNaN(n));
