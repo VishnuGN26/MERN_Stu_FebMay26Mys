2c0816604b605f21e29fa2d4128740368940a15a
VishnuGN26 <vishnugn145@gmail.com>
2026-03-03 15:43:27 +0530

    adding

---
+++ W3/D2/p7.js
@@ -0,0 +1,15 @@
+
+//arrow function
+(args) => {
+    //body of the function
+}
+//Add two numbers
+const Sum = (a,b) => {
+    return a+b;
+}
+console.log("3+5=",Sum(3,5));//calling function within console 
+//single line return /implict return 
+const square = x => x*x;
+console.log("Square of  44:",square(44));
+const sayHello = () => console.log("Hello from arrow func");
+sayHello();
