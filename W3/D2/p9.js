9d8fdeed0f8a87d3ac418f0c8f733c8267cbbd21
VishnuGN26 <vishnugn145@gmail.com>
2026-03-03 15:43:59 +0530

    adding

---
+++ W3/D2/p9.js
@@ -0,0 +1,16 @@
+//Immediately Invoked functionn expression(IIFE)
+//withoutparameter
+(function(){
+    console.log("Basic IIFE executed Immediately");
+})();
+//with parameter
+(function(appName,version){
+    console.log("App:",appName,"Version:",version);
+})("NodeJs","V22.22.0");
+
+//with return values
+const result = (function(){
+    const a = 10, b = 20;
+    return a+b;
+})();
+console.log("Sum is: ",result);
