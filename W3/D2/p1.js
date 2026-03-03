8698ba7fad3ba9b44c0d68fdbc903ec167294d13
VishnuGN26 <vishnugn145@gmail.com>
2026-03-03 15:41:46 +0530

    adding

---
+++ W3/D2/p1.js
@@ -0,0 +1,14 @@
+//Arithmetic operators
+console.log("Arithmetic Operators");
+ let a = 2, b = 3; 
+
+console.log("a+b: ",a+b);
+console.log("a-b: ",a-b);
+console.log("a*b: ",a*b);
+console.log("a/b: ",a/b);
+console.log("a%b: ",a%b); //modulas
+console.log("a**b: ",a**b);//exponential
+console.log("a++: ",a++); // post-increment
+console.log("++a: ",++a); //pre-increment
+console.log("a--: ",a--); //post-decrement
+console.log("--a: ",--a); //pre-decrement 
