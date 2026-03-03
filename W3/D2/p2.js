b960a5a35450431b459d6599eecd46c0b7aa2769
VishnuGN26 <vishnugn145@gmail.com>
2026-03-03 15:42:00 +0530

    adding

---
+++ W3/D2/p2.js
@@ -0,0 +1,14 @@
+//comparison operators
+console.log("Comparsion operators");
+let x=1,y='2';
+console.log("x==y: ",x==y); //losse equality
+console.log("x===y: ",x===y); //strict euqality
+console.log("x!=y: ",x!=y); //losse inequality
+console.log("x!==y: ",x!==y);//strict inequality
+console.log("x>3: " ,x>3);
+console.log("x<3: " ,x<3);
+console.log("x>=3: " ,x>=3);
+console.log("x<=3: " ,x<=3);
+
+
+
