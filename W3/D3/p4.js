bc07f0c59d51140fccf365857c3e4018d1dda37a
VishnuGN26 <vishnugn145@gmail.com>
2026-03-04 13:27:22 +0530

    adding

---
+++ W3/D3/p4.js
@@ -0,0 +1,6 @@
+//1.step size
+for(let i = 0;i<=10;i+2){
+    console.log("i=",i);
+}
+//2 two counters in one for loop
+for(let i = 1; j=5 
---
+++ W3/D3/p5.js
@@ -0,0 +1,12 @@
+
+//basics of objects
+const person = {
+    name: "Chiru",
+    
+    age: 28,
+    isStudent: false
+
+};
+//console.log("Person",person);
+console.log("Name:", person.name);
+console.log("Age:", person["age"]);
