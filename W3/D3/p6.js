f6fd61005ed81306f7cc86c15028e6b8bc9947ef
VishnuGN26 <vishnugn145@gmail.com>
2026-03-04 14:24:59 +0530

    adding

---
+++ W3/D3/p6.js
@@ -0,0 +1,21 @@
+//nested object and methods
+const student = {
+    fisrtName:"Chiru",
+    lastName:"HG",
+    scores:{
+        math: 80,
+        science: 83
+    },
+    hobbies:["reading","singing"],
+    fullname: function(){
+        return this.firstName + " " +this.lastName;
+    },
+
+
+greet(){
+    console.log("Hi, ",this.fullname());
+}
+};
+console.log(student.fullname());
+console.log("student",student);
+console.log(student.scores.math);//gives only the math scores of the student
