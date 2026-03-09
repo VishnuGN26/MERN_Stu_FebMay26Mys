d0be67a0690e04f6555e150a7d5031cec24eba9a
VishnuGN26 <vishnugn145@gmail.com>
2026-03-09 23:02:20 +0530

    adding

---
+++ W3/D5/p1.js
@@ -0,0 +1,10 @@
+//Basics of click event
+ const button = document.getElementById("clickBtn");
+//   button.addEventListener("click",function(){
+//     console.log("Button is clicked");
+//   });
+
+
+   button.addEventListener("dbclick",function(){
+    console.log("second event listner: Button is clicked");
+  });
