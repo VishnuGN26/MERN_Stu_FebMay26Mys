07c8b7f841da57fb38c23eb0d44747c32e2f8522
VishnuGN26 <vishnugn145@gmail.com>
2026-03-17 13:33:37 +0530

    callback error

---
+++ W5/D2/p5.js
@@ -0,0 +1,18 @@
+//callback error
+function divideNumber(a,b,callback){
+    if(b==0){
+        callback("cannot divide by zero.",null);
+        return;
+    }
+    const result = a/b;
+    callback(null,result);
+}
+divideNumber(10,2,function(error,result) {
+    if(error){
+        console.log("Error: ",error);
+        return;
+
+    }
+    console.log("Result: ",result);
+    
+})
