b4bc64c7f713457b56ddcaab2dd4489f9eb40836
VishnuGN26 <vishnugn145@gmail.com>
2026-03-17 13:31:02 +0530

    introduction to call back

---
+++ W5/D2/p1.js
@@ -0,0 +1,13 @@
+//introduction to callback function
+function greetUser(name,callback){
+    console.log("Hello ,"+name);
+    //the callback function is executed only after
+    //  the execution of the current function
+
+  callback();
+}
+function showCompletionMessage(){
+    console.log("The greeting task is conplete");
+}
+
+greetUser("Vishnu",showCompletionMessage);
