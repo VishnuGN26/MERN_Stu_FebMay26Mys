9c78dd6c8d27474347cc024febe6c706d0f14a36
VishnuGN26 <vishnugn145@gmail.com>
2026-03-18 10:42:43 +0530

    use async and await

---
+++ W5/D3/p2.js
@@ -0,0 +1,25 @@
+//why use async and await
+function getUser(){
+    return new Promise(function(resolve) {
+        setTimeout(function(){
+            resolve({id:101,name:"Vishnu"});
+        },0);
+        
+    });
+}
+function getOrders(userId){
+    return new Promise(function (resolve){
+    setTimeout(function(){
+        resolve(["order-A","order-B"]);
+    },12000);
+
+});
+}
+async function showUserAndOrders(){
+    const user = await getUser();
+    //console.log("User loaded: ",user.name);
+
+    const orders = await getOrders(user.id);
+    console.log("Orders loaded",orders);
+}
+showUserAndOrders();
