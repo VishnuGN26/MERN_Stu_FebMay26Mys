ed265dea6f430d18cad711ad1eb3f4685bd3af44
VishnuGN26 <vishnugn145@gmail.com>
2026-03-24 17:04:55 +0530

    removing eventemitter listeners

---
+++ W6/D2/p9.js
@@ -0,0 +1,19 @@
+//removing eventemitter listeners
+const EventEmitter = require("events");
+
+const jobEmitter = new EventEmitter();
+function showJobProgress(status){
+    console.log("Job Status: ",status);
+
+}
+//add listener
+jobEmitter.on("progress",showJobProgress);
+
+//emit the event
+jobEmitter.emit("progress","50% completed");
+
+//remove listener
+jobEmitter.off("progress",showJobProgress);
+
+//emit the event
+jobEmitter.emit("progress","100% completed");
