67595007ebd60c585959a0cfb75207677f35a1f6
VishnuGN26 <vishnugn145@gmail.com>
2026-04-09 23:32:29 +0530

    This file stores the movie data used in the CLI App

--- Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/data/movies.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/data/movies.js
@@ -1,55 +1,44 @@
-//movies.js
-//This file stores the movie data used in the CLI App
-
+// This file stores the movie data used in the CLI App
 const movies = [
     {
         id:1,
         title:"Dhurandar2",
         language:"Hindi",
-        genre:"action Thriller",
-        city: "Mysuru",
-        cinema: "Nexus Mall-Screen 1",
-        
+        genre:"Action Thriller",
+        city:"Mysuru",
+        cinema:"Nexus Mall-Screen 1",
         showtimes:[
-            {time: "10:00 AM",seatsAvailable: 250},
-            {time: "1:00 PM",seatsAvailable: 250},
-            {time: "6:00 PM",seatsAvailable: 250}
+            {id:1,time: "10:00 AM",seatsAvailable: 250},
+            {id:2,time: "1:00 PM",seatsAvailable: 250},
+            {id:3,time: "6:00 PM",seatsAvailable: 250}
         ]
     },
-
-{
+    {
         id:2,
         title:"LoveMocktail3",
         language:"Kannada",
         genre:"Romantic",
-        city: "Mysuru",
-        cinema: "DRC Mall-Screen 2",
-        
+        city:"Mysuru",
+        cinema:"DRC Mall-Screen 2",
         showtimes:[
-            {time: "10:00 AM",seatsAvailable: 100},
-            {time: "1:00 PM",seatsAvailable: 70},
-            {time: "6:00 PM",seatsAvailable: 200}
+            {id:1,time: "10:00 AM",seatsAvailable: 100},
+            {id:2,time: "1:00 PM",seatsAvailable: 70},
+            {id:3,time: "6:00 PM",seatsAvailable: 200}
         ]
     },
-
-
     {
         id:3,
-        title:"Haygreeva",
+        title:"Hayagreeva",
         language:"Kannada",
         genre:"Action",
-        city: "Davangere",
-        cinema: "Davangere Mall-Screen 1",
-        
+        city:"Davangere",
+        cinema:"Davangere Mall-Screen 1",
         showtimes:[
-            {time: "11:00 AM",seatsAvailable: 100},
-            {time: "2:00 PM",seatsAvailable: 70},
-            {time: "7:00 PM",seatsAvailable: 300}
+            {id:1,time: "11:00 AM",seatsAvailable: 100},
+            {id:2,time: "2:00 PM",seatsAvailable: 70},
+            {id:3,time: "7:00 PM",seatsAvailable: 200}
         ]
     }
 ];
-//Export the movie data so that other files are
-
+// Export the movie data so that other files can use it.
 module.exports = movies;
- 
-
