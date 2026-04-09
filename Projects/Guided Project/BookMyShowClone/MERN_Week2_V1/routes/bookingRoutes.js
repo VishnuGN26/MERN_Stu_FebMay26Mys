54c901418ba0a5fa3f1a89b2375929b67a585c46
VishnuGN26 <vishnugn145@gmail.com>
2026-04-09 10:20:01 +0530

    handling request related to bookings

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/routes/bookingRoutes.js
@@ -0,0 +1,19 @@
+//handling request related to bookings
+const express = require("express");
+const { authMiddleware } = require("../middleware/authMiddleware");
+const roleMiddleware = require("../middleware/roleMiddleware");
+const {
+    bookingValidationHandler,
+    createBooking,
+    getAllBookings,
+    getMyBookings
+} = require("../controllers/bookingController");
+const router = express.Router();
+
+//create a booking / to book ticket/s
+router.post("/bookings",authMiddleware,bookingValidationHandler,createBooking);
+
+router.get("/bookings/me",authMiddleware,getMyBookings);
+router.get("/bookings",authMiddleware,roleMiddleware("admin"),getAllBookings);
+
+module.exports = router; 
