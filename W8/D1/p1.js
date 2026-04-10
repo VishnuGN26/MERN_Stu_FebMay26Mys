b190980711ad678ca023f0f93586761d3a463b56
VishnuGN26 <vishnugn145@gmail.com>
2026-04-10 09:18:29 +0530

    connecting to MongoDb to NodeJS using mongoose and defining schema and modle

---
+++ W8/D1/p1.js
@@ -0,0 +1,27 @@
+//connecting to MongoDb to NodeJS using mongoose and defining schema and modle
+const mongoose = require("mongoose");
+async function main(){
+    try{
+        await mongoose.connect("mongodb://localhost:27017/abcmern");
+        console.log("MangoDB connected successfully");
+
+        const userSchema = new mongoose.Schema({
+            name: String,
+            age: Number,
+            role: String
+        });
+
+        const user = mongoose.model("User",userSchema);
+        console.log("mongoose schema & model created successfully");
+        console.log("Model name:",user.modelName);
+
+        await mongoose.connection.close();
+        console.log("connection closed");
+    }
+    catch(error){
+        console.log("Failed to connect to DB",error.message);
+    }
+}
+
+
+main();
