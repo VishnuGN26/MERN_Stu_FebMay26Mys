ef4a214256a880ef3afff1b66f1b90e69bbd5aae
VishnuGN26 <vishnugn145@gmail.com>
2026-04-21 12:26:02 +0530

    Created show schema,model and exported model

---
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/models/Show.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+const seatScheman = new mongoose.Schema({
+    seatNumber:{
+        type:String,
+        required:true,
+    },
+    isBooked:{
+        type:Boolean,
+        default:false,
+
+    },
+},{_id:false});
+
+const showSchema = new mongoose.Schema({
+    movieId:{
+        type:mongoose.schema.Types.objectId,
+        ref:"Movie",
+        index:true,
+    },
+    date:{
+        type:Date,
+        required:true,
+        index:true,
+
+    },
+    time:{
+        type:String,
+        required:true,
+    },
+    totalSeats:{
+        type:Number,
+        required:true,
+
+    },
+
+    availableSeats:{
+        type:Number,
+        required:true,
+    },
+    seats:{
+        type:[seatSchema],
+        required:true,
+    },
+    isActive:{
+        type:Boolean,
+        default:true,
+    },
+},{
+    timestamps:true,
+});
+
+//compound index:
+showSchema.index({movieId:1,date:1});
+
+//Add validation
+showSchema.pre("save",function(next){
+    if(this.availableSeats>this.totalSeats){
+        return next(new Error("Available seats cannot exceed total seats"));
+    }
+    next();
+
+});
+module.exports = mongoose.model("Show",showSchema);
