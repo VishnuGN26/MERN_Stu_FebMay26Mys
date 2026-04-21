b6d38e4e7d121f7c6715fd3df55a5920b9ee8b27
VishnuGN26 <vishnugn145@gmail.com>
2026-04-21 15:00:53 +0530

    Added auth routes

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
-
+const authRoutes = require("./routes");
 const app = express();
 
 app.use(express.json());
+app.use("/api/auth",authRoutes);
+
+//Base url
 app.get("/",(req,res)=>{
     res.status(200).json({
     success:true,
