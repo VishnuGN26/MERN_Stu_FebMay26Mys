670b5be515dbf033ef1e081bea1319b47ad2b057
VishnuGN26 <vishnugn145@gmail.com>
2026-04-22 13:06:23 +0530

    Role:middleware:RABC

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/middleware/role.middleware.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/middleware/role.middleware.js
@@ -1,13 +1,10 @@
-//role middleware: RBAC
-exports.authorize = (...roles)=>{
-    return(req,res,next)=>{
-        if(!res.user || !roles.includes(req.user.role)){
-            return res.status(403).json({
-                success:false,
-                message:"Access denied: insufficient permission",
-            });
-        }
-        next();
-    };
-
+//Role:middleware:RABC
+exports.authorize = (...roles) => (req, res, next) => {
+    if (!req.user || !roles.includes(req.user.role)) {
+        return res.status(403).json({
+            success: false,
+            message: "Access denied: insufficient permission",
+        });
+    }
+    next();
 };
