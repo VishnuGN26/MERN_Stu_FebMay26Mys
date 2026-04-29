d09d29281c8268d43f9f985b474ddd4d447291c5
VishnuGN26 <vishnugn145@gmail.com>
2026-04-29 12:43:43 +0530

    ObjectAndArrayProps added

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 // import { PropBasics } from "./components/P1"
-import { PropDestructuring } from "./components/P2"
-import { PropsChildren } from './components/P3'
-import { PropDrillingDemo } from './components/P4'
-import { SharedStateParent } from './components/P5'
+// import { PropDestructuring } from "./components/P2"
+// import { PropsChildren } from './components/P3'
+// import { PropDrillingDemo } from './components/P4'
+// import { SharedStateParent } from './components/P5'
+import { CompositionDemo } from './components/P6'
+import { ObjectAndArrayProps } from './components/P7'
 
 function App() {
 
@@ -13,7 +15,9 @@ function App() {
       {/* <PropDestructuring/> */}
       {/* <PropsChildren /> */}
       {/* <PropDrillingDemo /> */}
-      <SharedStateParent />
+      {/* <SharedStateParent /> */}
+      {/* < CompositionDemo /> */}
+      <ObjectAndArrayProps />
     </>
   )
 }
